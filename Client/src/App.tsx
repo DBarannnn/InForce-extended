5bcc3847ac3b4768fdeb60419d9cab8c34b9609d
Dmytro <bardmyt@gmail.com>
2023-10-27 20:18:49 +0300

    Table component preface

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,12 +2,13 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import './App.css'
 import Layout from './components/common/Layout'
+import Table from './components/Table'
 
 function App() {
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
-      <Route index element={<p>Table goes here</p>}/>
+      <Route index element={<Table />}/>
     </Route>
   ))
 
