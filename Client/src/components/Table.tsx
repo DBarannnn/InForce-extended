dc77c14a3ed2fd377215a23e2480ed632bc1dfdc
Dmytro <bardmyt@gmail.com>
2023-10-27 20:21:13 +0300

    Table reminder comments

--- Client/src/components/Table.tsx
+++ Client/src/components/Table.tsx
@@ -4,8 +4,17 @@ import {Form} from "react-router-dom"
 export default function Table(){
     return(
         <>
+            {/*
+            -Add loader to ensure security 
+            -Make req on server to get urls
+            -Map url into components
+            */
+            }
             <section className="table-section">
                 <h2>Add new URL</h2>
+                {/*
+                    Add action on this form to submit the link
+                */}
                 <Form className="add-url-form">
                     <input type="text" name="link" placeholder="Paste Your Long URL" className="url-input"></input>
                     <button className="shorten-btn">Shorten</button>
